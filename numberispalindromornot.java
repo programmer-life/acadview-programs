042a2408b67936f7a8c1b2f835cd0b5940a3a47e
salman <36587293+programmer-life@users.noreply.github.com>
2018-03-12 05:09:06 +0530

    Number is palindrome or not

---
+++ numberispalindromornot.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numberispalindromornot;
+
+/**
+ *
+ * @author Salman
+ */
+public class numberispalindromornot {
+    public static void main(String args[]){  
+  int r,sum=0,temp;    
+  int n=454;//It is the number variable to be checked for palindrome  
+  
+  temp=n;    
+  while(n>0){    
+   r=n%10;  //getting remainder  
+   sum=(sum*10)+r;    
+   n=n/10;    
+  }    
+  if(temp==sum)    
+   System.out.println("palindrome number ");    
+  else    
+   System.out.println("not palindrome");    
+}  
+    
+}
