7c3add917d8d8b008ae0fec59a4afa7c7955a6b9
salman <36587293+programmer-life@users.noreply.github.com>
2018-03-11 20:19:35 +0530

    sumoftwonumber

---
+++ sumoftwonumber.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Salman
+ */
+public class sumoftwonumber {
+    public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
+
