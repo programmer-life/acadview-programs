298a50bcab2c44c7a71b87834f3522f41aa52209
salman <36587293+programmer-life@users.noreply.github.com>
2018-03-12 06:36:24 +0530

    Given a sorted array A = {1,4,6,7,8,9,10}. Write a program to take a Number N from the user and check if it is in the array or not in least computation.

---
+++ arrayshort.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arrayornot;
+
+/**
+ *
+ * @author Salman
+ */
+public class arrayshort {
+    public static void main(String []args) 
+    {
+    Scanner sc=new Scanner(System.in);
+    int ar[]={1,4,6,7,8,9,10};
+    System.out.println("Enter Number Search:");
+    int num=sc.nextInt();
+    for(int i=0;i<=ar[i];i++){
+    
+        if(num==ar[i])
+            System.out.println(num+" is store at index location "+(i+1));
+ 
+                              }
+    }
+    
+}
